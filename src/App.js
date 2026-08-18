7fb163e9650d2f281b065cb25dea39c9ac6443bd
pavithrapramod <pavithrapramodk@gmail.com>
2026-08-18 10:39:48 +0530

    Import Header and Todos components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import dayjs from 'dayjs';
+import Header from './components/Header';
+import Todos from './components/Todos';
 
 function App() {
   // Existing state data schemas (Preserved intact)
