80a9ff8d30ba9f9fa9586053c7822397c0567d1a
pavithrapramod <pavithrapramodk@gmail.com>
2026-08-18 10:18:33 +0530

    Update todo titles in App.js

--- src/App.js
+++ src/App.js
@@ -9,19 +9,19 @@ function App() {
     const todoData = [
         {
             _id: 1,
-            title: 'Read boyd language book',
+            title: 'Complete Coud Computing lab',
             status: false,
             deadline: dayjs('2020-09-18T17:11:54'),
         },
         {
             _id: 2,
-            title: 'Do My Home Work',
+            title: 'Revise java inheritance',
             status: true,
             deadline: dayjs('2012-02-18T10:16:04'),
         },
         {
             _id: 3,
-            title: 'create mini project react',
+            title: 'Submit dbms assignment',
             status: false,
             deadline: dayjs('2018-08-18T21:11:54'),
         },
